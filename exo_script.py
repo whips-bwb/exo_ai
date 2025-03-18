2035df11590048c83e90bf425934e224e1e60adf
whips <whips@musinux.com>
2025-03-18 14:31:42 +0000

    init & first script commit

---
+++ exo_script.py
@@ -0,0 +1,16 @@
+# import os, sys
+
+
+def example_function():
+    # This function has bad indentation
+    print("Hello world!")
+
+
+def another_function():
+    a = 10  # Missing spaces around the assignment operator
+    print(a)
+
+
+if __name__ == "__main__":
+    example_function()
+    another_function()
