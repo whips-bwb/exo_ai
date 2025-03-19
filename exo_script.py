85fe72f9697178c05334e0080dcdc92561bdf58a
whips <whips@musinux.com>
2025-03-19 16:17:57 +0000

    error correction

--- exo_script.py
+++ exo_script.py
@@ -1,12 +1,15 @@
 # import os, sys
 
+
 def example_function():
     # This function has bad indentation
     print("Hello world!")
 
+
 def another_function():
     a = 10  # Missing spaces around the assignment operator
     print(a)
+
 
 if __name__ == "__main__":
     example_function()
