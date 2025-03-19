55fa80be033d4262eb51567141c402851a549116
whips <whips@musinux.com>
2025-03-19 15:46:34 +0100

    Update exo_script.py

--- exo_script.py
+++ exo_script.py
@@ -3,11 +3,11 @@
 
 def example_function():
     # This function has bad indentation
-    print("Hello world!")
+print("Hello world!")
 
 
 def another_function():
-    a = 10  # Missing spaces around the assignment operator
+    a=10  # Missing spaces around the assignment operator
     print(a)
 
 
