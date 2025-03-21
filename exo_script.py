7aa496cff4730cee2d26c7035feef7a3e8500fb9
whips <whips@musinux.com>
2025-03-21 18:04:42 +0000

    second exo

--- exo_script.py
+++ exo_script.py
@@ -1,5 +1,35 @@
 # import os, sys
 
+def process_data(data):
+    if isinstance(data, list) == True:
+        process_list(data)
+    elif isinstance(data, dict):
+        process_dict(data)
+    else:
+        print("Unsupported data type")
+
+def process_list(list_data):
+    for i in range(len(list_data)):
+        print(list_data[i])
+
+def process_dict(dict_data):
+    for key in dict_data:
+        print(key, dict_data[key])
+
+data = [1, 2, 3]
+process_data(data)
+
+# Some complex if-else logic with redundant conditions
+def is_number_positive(numb):
+    if numb > 0:
+        result = "positive"
+    elif numb == 0:
+        result = "zero"
+    elif numb < 0:
+    result = "negative"  # Incorrect indentation
+    return result
+
+print(is_number_positive(-5))  # Check with negative number
 
 def example_function():
     # This function has bad indentation
